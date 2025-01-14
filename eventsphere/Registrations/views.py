be5f84434b46ecf507ee15071ced8c1e548af569
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-01-14 21:48:56 +0530

    feat: enhance login functionality with user authentication and error messaging

--- eventsphere/Registrations/views.py
+++ eventsphere/Registrations/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login  # Import the login function
 from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-def login_user(request):
 
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")  # Redirect to a home page or dashboard
+    
+        else:
+            messages.error(request, "Invalid username or password.")
+            return render(request, "registration/login.html")
+     
+    else:
+        return render(request, "registration/login.html")
 
 
 def registration(request):
@@ -13,12 +28,14 @@ def registration(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            username = form.cleaned_data.get('username')
+            username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}!")
             auth_login(request, user)
             return redirect("home")  # Redirect to a home page or dashboard
         else:
-            messages.error(request, "Registration failed. Please correct the errors below.")
+            messages.error(
+                request, "Registration failed. Please correct the errors below."
+            )
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {"form": form})
