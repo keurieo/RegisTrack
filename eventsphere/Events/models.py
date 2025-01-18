4f31617fcaa350a1a271764319f8cab83c8689e3
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-01-18 15:11:34 +0530

    feat: add Event model and register it in admin for event management

--- eventsphere/Events/models.py
+++ eventsphere/Events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    short_description = models.TextField()
+    criteria = models.TextField()
+    image = models.ImageField(upload_to='event_images/')
+    organizer = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    max_attendees = models.PositiveIntegerField()
+    is_free = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
